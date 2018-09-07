493d132859fba8b061c6e4828e3abebff53a4a55
joancurrie <jecurrie8@gmail.com>
2018-09-07 11:51:03 -0400

    Update app.js

--- app.js
+++ app.js
@@ -5,12 +5,7 @@ import ReactDOM from 'react-dom';
 const math =
   <div> 
     <h1>2 + 3 = {2 + 3}</h1>;  
-    <h1>
-      PI, YALL!
-    </h1>
-    <p>
-      {Math.PI.toFixed(20)}
-    </p>
+
   </div>
 
 ReactDOM.render(math, document.getElementById('app')
